92107a9718daaa0bb2f7ee34124ebbf88f16770e
Ergiaa <titovirgiawan21@gmail.com>
2025-08-21 23:09:18 +0700

    config: removing zsh init from configs and use default instead

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -18,7 +18,7 @@ M.base46 = {
 M.nvdash = { load_on_startup = true }
 M.ui = {
   statusline = {
-    theme = "vscode_colored",
+    theme = "vscode",
   },
   --  tabufline = {
   --     lazyload = false
