ac4c86fab315cf097bc9a110c63d109ed0d290d3
Ergiaa <titovirgiawan21@gmail.com>
2025-08-26 10:15:15 +0700

    config: changed render-markdown preset

--- .config/nvim/lua/configs/render-markdown.lua
+++ .config/nvim/lua/configs/render-markdown.lua
@@ -4,9 +4,17 @@ local render = require "render-markdown"
 ---@type rnder.md.UserConfig
 render.setup {
   -- Your options here
+  preset = "obsidian",
   completions = {
     lsp = {
       enabled = true,
     },
+  },
+  heading = {
+    border = true,
+  },
+  indent = {
+    enabled = true,
+    skip_heading = true,
   },
 }
